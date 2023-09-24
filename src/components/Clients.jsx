d580405119eeebafc19a5414a52ad39aba5a5371
hsuifang <whalekiller03@gmail.com>
2023-09-24 18:34:30 +0800

    Add Client, CTA, FeedBackCard, Testimonials and Footer section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../styles";
 
-function Clients() {
-  return (
-    <div>
-      Clients
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 flex-wrap ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] min-h-[60px] client-container`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+          <div className="w-full" />
+          <div className="flex justify-center h-1 mt-5 client-container__line"></div>
+        </div>
+      ))}
     </div>
-  )
-}
+  </section>
+);
 
-export default Clients
+export default Clients;
--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { quotes } from "../assets";
 
-function FeedbackCard() {
-  return <div>FeedbackCard</div>;
+function FeedbackCard({ content, name, title, img }) {
+  return (
+    <div className="flex justify-between flex-col px-10 py-[60px] rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="font-poppins font-normal text-white leading-[32px] text-[18px] my-10">
+        {content}
+      </p>
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semiBold text-white leading-[32px] text-[20px]">
+            {name}
+          </h4>
+          <p className="font-poppins font-semiBold text-dimWhite leading-[32px] text-[16px]">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FeedbackCard;
